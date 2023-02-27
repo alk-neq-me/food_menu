c9950234f80f2f37e10cef2232543dfd89ff7fb4
alk-neq-me <toyko2001@gmail.com>
2023-02-27 19:24:03 +0630

    inint

--- food_menu/components/Footer.tsx
+++ food_menu/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<div className="absolute left-0 w-full text-white p-5 flex flex-col items-center justify-center bg-[#7c2d12]">
+		<div className="absolute text-center left-0 w-full text-white p-5 flex flex-col items-center justify-center bg-[#7c2d12]">
 			<p>Design by the @abcd technologiest illustrations by abcd</p>
 		</div>
 	);
